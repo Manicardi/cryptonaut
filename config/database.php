3746429dbc84082837e33782782c508dfe4fa08a
Gabriel <gabrieltrovatto@hotmail.com>
2023-08-25 15:18:26 -0300

    Ajustado nome db

--- config/database.php
+++ config/database.php
@@ -125,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'CryptonautCoin'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'Cryptonaut'), '_').'_database_'),
         ],
 
         'default' => [
