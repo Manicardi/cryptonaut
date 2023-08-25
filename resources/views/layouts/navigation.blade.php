972230e8a716b22ec996fe2a33372a8da2987ab7
Gabriel <gabrieltrovatto@hotmail.com>
2023-08-25 17:57:25 -0300

    Removido link de get do logout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -86,7 +86,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <x-dropdown-link :href="route('logout')"
+                                <x-dropdown-link :href="route('/')"
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
                                     {{ __('Log Out') }}
