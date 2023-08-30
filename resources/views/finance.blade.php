140bbebb0b73339d059154b9be4dcdbb08c5b535
Gabriel <gabrieltrovatto@hotmail.com>
2023-08-30 17:54:37 -0300

    Ajustado template do finance

--- resources/views/finance.blade.php
+++ resources/views/finance.blade.php
@@ -9,7 +9,7 @@
         <div class="grid grid-cols-1 md:grid-cols-1 gap-1 lg:gap-1">
             <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                 <div class=" text-gray-900 dark:text-gray-100">
-                    <form method="post" action="{{ route('requestWithdraw') }}" class="space-y-6">
+                    <form method="post" action="{{ route('requestWithdraw') }}">
                         @csrf
                         <header>
                             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
